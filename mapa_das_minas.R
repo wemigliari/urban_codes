60f1809d8f8b0e118fcc4644b6d604bf5e9dbd03
Wellington Migliari <49842956+wemigliari@users.noreply.github.com>
2021-03-20 22:50:54 +0100

    Add files via upload

---
+++ mapa_das_minas.R
@@ -0,0 +1,116 @@
+library(knitr)
+library(writexl)
+library(rgdal)
+library(sf) ## Read shapefiles
+library(dplyr)
+library(leaflet)
+library(cowplot)
+library(ggplot2)
+library(plotly)
+library(readxl)
+library(htmlwidgets)
+
+mineracao <- read_sf("/Users/wemigliari/Documents/HTML/polos_data/img/Belo Horizonte.shp",
+                    stringsAsFactors = TRUE) ### Shape in form of sf
+
+
+
+mina_ma <- read_sf("/Users/wemigliari/Documents/HTML/polos_data/img/Mina Mar Azul1.shp")
+mina_mu <- read_sf("/Users/wemigliari/Documents/HTML/polos_data/img/Mina da Mutuca.shp")
+mina_mc <- read_sf("/Users/wemigliari/Documents/HTML/polos_data/img/Mina Morro do Chapéu.shp")
+mina_cp <- read_sf("/Users/wemigliari/Documents/HTML/polos_data/img/Mina de Capão.shp")
+mina_abo <- read_sf("/Users/wemigliari/Documents/HTML/polos_data/img/Mina Abóboras.shp")
+mina_acaba <- read_sf("/Users/wemigliari/Documents/HTML/polos_data/img/Mina Acaba Mundo.shp")
+mina_cdm <- read_sf("/Users/wemigliari/Documents/HTML/polos_data/img/Mina Capitão do Mato.shp")
+mina_casa_pedra <- read_sf("/Users/wemigliari/Documents/HTML/polos_data/img/Mina Casa de Pedra.shp")
+mina_cascalheira <- read_sf("/Users/wemigliari/Documents/HTML/polos_data/img/Mina Cascalheira.shp")
+mina_central_brasil <- read_sf("/Users/wemigliari/Documents/HTML/polos_data/img/Mina Central do Brasil.shp")
+mina_cofins <- read_sf("/Users/wemigliari/Documents/HTML/polos_data/img/Mina Cofins.shp")
+mina_corrego_feijao <- read_sf("/Users/wemigliari/Documents/HTML/polos_data/img/Mina Córrego do Feijão.shp")
+
+
+
+leaflet() %>% 
+  addTiles() %>% 
+  addPolygons(data=mina_abo, weight = 1,
+              fillColor = "green", 
+              color = "green",
+              fillOpacity = 0.2, 
+              label = "Mina Abóboras",
+              group = "Mina Abóboras")%>%
+  addPolygons(data=mina_acaba, weight = 1, 
+              fillColor = 'turquoise',
+              color = "turquoise",
+              label = "Mina Acaba Mundo",
+              group = "Mina Acaba Mundo",
+              fillOpacity = 0.8)%>%
+  addPolygons(data=mina_central_brasil, weight = 1,
+              fillColor = "#97B1C2", 
+              color = "#97B1C2",
+              fillOpacity = 0.2, 
+              label = "Mina Central do Brasil",
+              group = "Mina Central do Brasil")%>%
+  addPolygons(data=mina_cascalheira, weight = 1,
+              fillColor = "#133B56", 
+              color = "#133B56",
+              fillOpacity = 0.2, 
+              label = "Mina Cascalheira",
+              group = "Mina Cascalheira")%>%
+  addPolygons(data=mineracao, weight = 1,
+              fillColor = "olive", 
+              color = "olive",
+              fillOpacity = 0.2, 
+              label = mineracao$NOME)%>%
+  addPolygons(data=mina_abo, weight = 1, 
+              fillColor = "darkgreen", 
+              color = "darkgreen",
+              fillOpacity = 0.2, 
+              label = "Mina Capitão do Mato",
+              group = "Mina Capitão do Mato")%>%
+  addPolygons(data=mina_ma$geometry, weight = 1, 
+              fillColor = 'gold',
+              color="gold",
+              label = "Mina Mar Azul, Vale S/A",
+              group = "Mina Mar Azul",
+              fillOpacity = 0.8)%>%
+  addPolygons(data=mina_mc, weight = 1, 
+              fillColor = 'green',
+              color = "green",
+              label = "Mina do Chapéu, Vale S/A",
+              group = "Mina do Chapéu",
+              fillOpacity = 0.8)%>%
+  addPolygons(data=mina_cp, weight = 1, 
+              fillColor = 'purple',
+              color="purple",
+              label = "Mina de Capão, Vale S/A",
+              group = "Mina de Capão",
+              fillOpacity = 0.8)%>%
+  addPolygons(data=mina_mu, weight = 1, 
+              fillColor = 'steelblue',
+              color = "steelblue",
+              label = "Mina da Mutuca, Vale S/A",
+              group = "Mina da Mutuca",
+              fillOpacity = 0.8)%>%
+  addPolygons(data=mina_casa_pedra, weight = 1, 
+              fillColor = 'blue',
+              color = "blue",
+              label = "Mina Casa de Pedra",
+              group = "Mina Casa de Pedra",
+              fillOpacity = 0.8)%>%
+  addProviderTiles(providers$Stamen.TonerLite)%>% 
+  setView(lat= -19.983333, lng = -43.940933, zoom = 11)%>%
+  addLayersControl(
+    overlayGroups = c("Mina Cascalheira",
+                      "Mina Central do Brasil",
+                      "Mina Abóboras", 
+                      "Mina Capitão do Mato", 
+                      "Mina Acaba Mundo",
+                      "Mina Mar Azul", 
+                      "Mina do Chapéu", 
+                      "Mina da Mutuca", 
+                      "Mina de Capão",
+                      "Mina Casa de Pedra"),
+    options = layersControlOptions(collapsed = FALSE)
+  )
+
+  
